eb592a4bbfa69a9c3bc87347a8c5688d063f282e
jackvdp <jack@pumpymusic.co.uk>
2024-08-30 19:03:50 +0100

    Extract footer and remove unnecessary jquery

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import NavBar from './resuables/NavBar';
 import MouseCursor from './resuables/MouseCursor';
 import Preloader from './resuables/Preloader';
+import Footer from "./resuables/Footer";
 import '../assets/css/owl.carousel.css'
 
 function About() {
@@ -20,14 +21,6 @@ function About() {
                 // Initialize plugins after script is loaded
                 $('.vertical-tabs-5c9381c4295a1').pt_tabs();
 
-                $('.banner-area-5c9cadf8f745e').find('.video').each(function () {
-                    $(this)[0].play();
-                });
-
-                $('.banner-area-65bfe324f043e').find('.video').each(function () {
-                    $(this)[0].play();
-                });
-
                 $('.team-carousel-5c938618203d9').each(function () {
                     var $this = $(this);
 
@@ -43,7 +36,7 @@ function About() {
                             autoplayHoverPause: true,
                             navClass: ['owl-prev free-basic-ui-elements-left-arrow', 'owl-next free-basic-ui-elements-right-arrow'],
                             navText: false,
-                            margin: 1,
+                            margin: 10,
                             responsive: {
                                 0: { items: 1 }, 576: { items: 2 }, 768: { items: 2 }, 992: { items: 2 }, 1200: { items: 2 },
                             }
@@ -321,78 +314,7 @@ function About() {
                         </div>
                     </div>
                 </main>
-                <footer className="site-footer">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-12 col-sm-6 col-lg-6">
-                                <div className="site-logo"><a href="/"><img className="light" src="/assets/images/logo.webp" alt="Elev8 FX" /><img
-                                    className="dark" src="/assets/images/logo.webp" alt="Elev8 FX" /></a></div>
-                                <div id="black-studio-tinymce-3" className="widget widget_black_studio_tinymce">
-                                    <div className="textwidget">
-                                        <p style={{ marginTop: "0", fontSize: "8px", lineHeight: "12px" }}><span
-                                            style={{ fontSize: "8pt", color: "#808080" }}>Elev8 Group has carefully selected industry leading fintech
-                                            and foreign exchange partners to provide our clients with regulated services and
-                                            products:</span><br />
-                                            <span style={{ fontSize: "8pt", color: "#808080" }}>Elev8 Group work with leading fintech companies and
-                                                foreign exchange partners to provide regulated services for our customers. Payment services for
-                                                Elev8 Group are provided by Sciopay Ltd. Sciopay Ltd is a company incorporated in England &amp;
-                                                Wales. Registration No: 12352935. Sciopay Ltd is licensed and regulated by HMRC as a Money Service
-                                                Business (MSB). License No: XCML00000151326. Sciopay Ltd is authorised by the Financial Conduct
-                                                Authority as an Authorised Payment Institution. Firm Reference Number: 927951</span><br />
-                                            <span style={{ fontSize: "8pt", color: "#808080" }}>Elev8 Group Limited’s Payment and Foreign Currency
-                                                Exchange Services are provided by Ebury Partners UK Limited. Elev8 Group Limited is partnered with
-                                                Ebury Partners UK Limited as a Programme Manager. Ebury Partners UK Limited is authorised and
-                                                regulated by the Financial Conduct Authority as an Electronic Money Institution (Financial Services
-                                                Register No. 900797). Ebury Partners UK Ltd is registered with the Information Commissioner’s
-                                                Office, with registration number: ZA345828</span><br />
-                                            <span style={{ fontSize: "8pt", color: "#808080" }}>Ebury Partners UK Limited (EPUK) is an Authorised
-                                                Electronic Money Institution and is a licensed entity that is permitted to offer spots and forwards
-                                                for commercial purposes of facilitating payment for identifiable goods or services and direct
-                                                investments, as per the applicable Markets in Financial Instruments Directive II (MiFID) exemption.
-                                                Regulated investment products, as defined in MiFID II, fall out of the scope of this licence and are
-                                                not permitted to be traded under EPUK.</span><br />
-                                            <span style={{ fontSize: "8pt", color: "#808080" }}>For clients based in the United Kingdom and rest of the
-                                                world, payment services for Elev8 Group Ltd are provided by The Currency Cloud Limited. Registered
-                                                in England and Wales No. 06323311. Registered Office: Stewardship Building 1st Floor, 12 Steward
-                                                Street London E1 6FQ. The Currency Cloud Limited is authorised by the Financial Conduct Authority
-                                                under the Electronic Money Regulations 2011 for the issuing of electronic money (FRN:
-                                                900199).</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="hide">
-                            </div>
-                            <div className="col-12 col-sm-2">
-                            </div>
-                            <div className="col-12 col-sm-4">
-                                <div id="custom_html-1" className="widget_text widget widget_custom_html">
-                                    <h6 className="widget-title">Contacts</h6>
-                                    <div className="textwidget custom-html-widget">
-                                        <div className="contact-row">
-                                            <i className="base-icon-phone-call"></i>
-                                            <a href="tel:+442045801415"><span>+44 (0) 20 4580 1415</span></a>
-                                        </div>
-                                        <div className="contact-row">
-                                            <i className="base-icon-envelope"></i>
-                                            <a href="mailto:info@elev8exchange.com"><span>info@elev8exchange.com</span></a>
-                                        </div>
-                                        <div className="contact-row">
-                                            <i className="base-icon-placeholder"></i>
-                                            <span>1 Moor Place, 1 Fore Street Avenue, London EC2Y 9DT</span>
-                                        </div>
-                                        <div className="contact-row">
-                                            <a href="/complaints"><span>Complaints</span></a>
-                                        </div>
-                                        <div className="contact-row">
-                                            <a href="/safeguar"><span>Safeguarding Funds</span></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+                <Footer />
             </div>
         </div>
     )
