6d30311197a07b33b4829a0273431af4f676a30e
jackvdp <jack@pumpymusic.co.uk>
2024-08-30 19:06:47 +0100

    Set up services component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Services from './components/Services';
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />} />
             </Routes>
         </Router>
     );
