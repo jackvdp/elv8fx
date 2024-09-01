bf1385e0e1421b784f881666825423f6b18803ee
jackvdp <jack@pumpymusic.co.uk>
2024-09-01 09:21:44 +0100

    Center disclaimer text

--- src/components/resuables/Footer.js
+++ src/components/resuables/Footer.js
@@ -94,35 +94,35 @@ function Contacts() {
 function DisclaimerText() {
     return (
         <div id="black-studio-tinymce-3" className="widget widget_black_studio_tinymce">
-            <div className="textwidget">
+            <div className="textwidget-center-widget">
                 <p style={{ marginTop: "0", fontSize: "8px", lineHeight: "12px" }}><span
                     style={{ fontSize: "8pt", color: "#808080" }}>Elev8 Group has carefully selected industry leading fintech
                     and foreign exchange partners to provide our clients with regulated services and
-                    products:</span><br />
-                    <span style={{ fontSize: "8pt", color: "#808080" }}>Elev8 Group work with leading fintech companies and
-                        foreign exchange partners to provide regulated services for our customers. Payment services for
-                        Elev8 Group are provided by Sciopay Ltd. Sciopay Ltd is a company incorporated in England &amp;
-                        Wales. Registration No: 12352935. Sciopay Ltd is licensed and regulated by HMRC as a Money Service
-                        Business (MSB). License No: XCML00000151326. Sciopay Ltd is authorised by the Financial Conduct
-                        Authority as an Authorised Payment Institution. Firm Reference Number: 927951</span><br />
-                    <span style={{ fontSize: "8pt", color: "#808080" }}>Elev8 Group Limited’s Payment and Foreign Currency
-                        Exchange Services are provided by Ebury Partners UK Limited. Elev8 Group Limited is partnered with
-                        Ebury Partners UK Limited as a Programme Manager. Ebury Partners UK Limited is authorised and
-                        regulated by the Financial Conduct Authority as an Electronic Money Institution (Financial Services
-                        Register No. 900797). Ebury Partners UK Ltd is registered with the Information Commissioner’s
-                        Office, with registration number: ZA345828</span><br />
-                    <span style={{ fontSize: "8pt", color: "#808080" }}>Ebury Partners UK Limited (EPUK) is an Authorised
-                        Electronic Money Institution and is a licensed entity that is permitted to offer spots and forwards
-                        for commercial purposes of facilitating payment for identifiable goods or services and direct
-                        investments, as per the applicable Markets in Financial Instruments Directive II (MiFID) exemption.
-                        Regulated investment products, as defined in MiFID II, fall out of the scope of this licence and are
-                        not permitted to be traded under EPUK.</span><br />
-                    <span style={{ fontSize: "8pt", color: "#808080" }}>For clients based in the United Kingdom and rest of the
-                        world, payment services for Elev8 Group Ltd are provided by The Currency Cloud Limited. Registered
-                        in England and Wales No. 06323311. Registered Office: Stewardship Building 1st Floor, 12 Steward
-                        Street London E1 6FQ. The Currency Cloud Limited is authorised by the Financial Conduct Authority
-                        under the Electronic Money Regulations 2011 for the issuing of electronic money (FRN:
-                        900199).</span>
+                    products:<br />
+                    Elev8 Group work with leading fintech companies and
+                    foreign exchange partners to provide regulated services for our customers. Payment services for
+                    Elev8 Group are provided by Sciopay Ltd. Sciopay Ltd is a company incorporated in England &amp;
+                    Wales. Registration No: 12352935. Sciopay Ltd is licensed and regulated by HMRC as a Money Service
+                    Business (MSB). License No: XCML00000151326. Sciopay Ltd is authorised by the Financial Conduct
+                    Authority as an Authorised Payment Institution. Firm Reference Number: 927951<br />
+                    Elev8 Group Limited’s Payment and Foreign Currency
+                    Exchange Services are provided by Ebury Partners UK Limited. Elev8 Group Limited is partnered with
+                    Ebury Partners UK Limited as a Programme Manager. Ebury Partners UK Limited is authorised and
+                    regulated by the Financial Conduct Authority as an Electronic Money Institution (Financial Services
+                    Register No. 900797). Ebury Partners UK Ltd is registered with the Information Commissioner’s
+                    Office, with registration number: ZA345828<br />
+                    Ebury Partners UK Limited (EPUK) is an Authorised
+                    Electronic Money Institution and is a licensed entity that is permitted to offer spots and forwards
+                    for commercial purposes of facilitating payment for identifiable goods or services and direct
+                    investments, as per the applicable Markets in Financial Instruments Directive II (MiFID) exemption.
+                    Regulated investment products, as defined in MiFID II, fall out of the scope of this licence and are
+                    not permitted to be traded under EPUK.<br />
+                    For clients based in the United Kingdom and rest of the
+                    world, payment services for Elev8 Group Ltd are provided by The Currency Cloud Limited. Registered
+                    in England and Wales No. 06323311. Registered Office: Stewardship Building 1st Floor, 12 Steward
+                    Street London E1 6FQ. The Currency Cloud Limited is authorised by the Financial Conduct Authority
+                    under the Electronic Money Regulations 2011 for the issuing of electronic money (FRN:
+                    900199).</span>
                 </p>
             </div>
         </div>
